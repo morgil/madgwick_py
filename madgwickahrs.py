112ca2d2bc1cff163feb8eeb6cc1400708e74c0f
Jonas Böer <jonas@morgil.de>
2015-09-02 15:19:14 +0200

    * fix for data types * assertion for non-zero input values changed to warning * fixed array slicing * small stuff

--- madgwickahrs.py
+++ madgwickahrs.py
@@ -1,4 +1,4 @@
-
+import warnings
 import numpy as np
 from numpy.linalg import norm
 from madgwick_py.quaternion import Quaternion
@@ -20,20 +20,24 @@ class MadgwickAHRS:
     def update(self, gyroscope, accelerometer, magnetometer):
         q = self.quaternion
 
-        gyroscope = np.array(gyroscope)
-        accelerometer = np.array(accelerometer)
-        magnetometer = np.array(magnetometer)
+        gyroscope = np.array(gyroscope, dtype=float)
+        accelerometer = np.array(accelerometer, dtype=float)
+        magnetometer = np.array(magnetometer, dtype=float)
 
         # Normalise accelerometer measurement
-        assert norm(accelerometer) != 0
+        if norm(accelerometer) is 0:
+            warnings.warn("accelerometer is zero")
+            return
         accelerometer /= norm(accelerometer)
 
         # Normalise magnetometer measurement
-        assert norm(magnetometer) != 0
+        if norm(magnetometer) is 0:
+            warnings.warn("magnetometer is zero")
+            return
         magnetometer /= norm(magnetometer)
 
         h = q * (Quaternion(0, magnetometer[0], magnetometer[1], magnetometer[2]) * q.inv())
-        b = np.array([0, norm(h[1:2]), 0, h[3]])
+        b = np.array([0, norm(h[1:3]), 0, h[3]])
 
         # Gradient descent algorithm corrective step
         f = np.array([
@@ -42,7 +46,7 @@ class MadgwickAHRS:
             2*(0.5 - q[1]**2 - q[2]**2) - accelerometer[2],
             2*b[1]*(0.5 - q[2]**2 - q[3]**2) + 2*b[3]*(q[1]*q[3] - q[0]*q[2]) - magnetometer[0],
             2*b[1]*(q[1]*q[2] - q[0]*q[3]) + 2*b[3]*(q[0]*q[1] + q[2]*q[3]) - magnetometer[1],
-            2*b[1]*(q[0]*q[2] + q[1]*q[3]) + 2*b[3]*(0.5 - q[1]**2 - q[2] ** 2) - magnetometer[2]
+            2*b[1]*(q[0]*q[2] + q[1]*q[3]) + 2*b[3]*(0.5 - q[1]**2 - q[2]**2) - magnetometer[2]
         ])
         j = np.array([
             [-2*q[2],                  2*q[3],                  -2*q[0],                  2*q[1]],
@@ -84,7 +88,7 @@ class MadgwickAHRS:
         step /= norm(step)  # normalise step magnitude
 
         # Compute rate of change of quaternion
-        qdot = 0.5 * (q * Quaternion(0, gyroscope[0], gyroscope[1], gyroscope[2])) - self.beta * step.T
+        qdot = (q * Quaternion(0, gyroscope[0], gyroscope[1], gyroscope[2])) * 0.5 - self.beta * step.T
 
         # Integrate to yield quaternion
         q += qdot * self.samplePeriod
