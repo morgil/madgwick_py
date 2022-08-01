fix quaternion interface

quaternion.py
@@ -45,7 +45,7 @@ class Quaternion:
             if len(q) != 4:
                 raise ValueError("Expecting a 4-element array or w x y z as parameters")
 
-        self._set_q(q)
+        self.q = q
 
     # Quaternion specific interfaces
 
@@ -63,9 +63,10 @@ class Quaternion:
         If the quaternion is the identity quaternion (1, 0, 0, 0), a rotation along the x axis with angle 0 is returned.
         :return: rad, x, y, z
         """
-        if self.__dict__[0] == 1 and self.__dict__[1] == 0 and self.__dict__[2] == 0 and self.__dict__[3] == 0:
+        if self[0] == 1 and self[1] == 0 and self[2] == 0 and self[3] == 0:
             return 0, 1, 0, 0
-        rad = np.arccos(self.__dict__[0]) * 2
+        rad = np.arccos(self[0]) * 2
+        imaginary_factor = np.sin(rad / 2)
         if abs(imaginary_factor) < 1e-8:
             return 0, 1, 0, 0
         x = self._q[1] / imaginary_factor
@@ -79,22 +80,22 @@ class Quaternion:
         return Quaternion(np.cos(rad / 2), x*s, y*s, z*s)
 
     def to_euler_angles(self):
-        pitch = np.arcsin(2 * self.__dict__[1] * self.__dict__[2] + 2 * self.__dict__[0] * self.__dict__[3])
-        if np.abs(self.__dict__[1] * self.__dict__[2] + self.__dict__[3] * self.__dict__[0] - 0.5) < 1e-8:
+        pitch = np.arcsin(2 * self[1] * self[2] + 2 * self[0] * self[3])
+        if np.abs(self[1] * self[2] + self[3] * self[0] - 0.5) < 1e-8:
             roll = 0
-            yaw = 2 * np.arctan2(self.__dict__[1], self.__dict__[0])
-        elif np.abs(self.__dict__[1] * self.__dict__[2] + self.__dict__[3] * self.__dict__[0] + 0.5) < 1e-8:
-            roll = -2 * np.arctan2(self.__dict__[1], self.__dict__[0])
+            yaw = 2 * np.arctan2(self[1], self[0])
+        elif np.abs(self[1] * self[2] + self[3] * self[0] + 0.5) < 1e-8:
+            roll = -2 * np.arctan2(self[1], self[0])
             yaw = 0
         else:
-            roll = np.arctan2(2 * self.__dict__[0] * self.__dict__[1] - 2 * self.__dict__[2] * self.__dict__[3], 1 - 2 * self.__dict__[1] ** 2 - 2 * self.__dict__[3] ** 2)
-            yaw = np.arctan2(2 * self.__dict__[0] * self.__dict__[2] - 2 * self.__dict__[1] * self.__dict__[3], 1 - 2 * self.__dict__[2] ** 2 - 2 * self.__dict__[3] ** 2)
+            roll = np.arctan2(2 * self[0] * self[1] - 2 * self[2] * self[3], 1 - 2 * self[1] ** 2 - 2 * self[3] ** 2)
+            yaw = np.arctan2(2 * self[0] * self[2] - 2 * self[1] * self[3], 1 - 2 * self[2] ** 2 - 2 * self[3] ** 2)
         return roll, pitch, yaw
 
     def to_euler123(self):
-        roll = np.arctan2(-2 * (self.__dict__[2] * self.__dict__[3] - self.__dict__[0] * self.__dict__[1]), self.__dict__[0] ** 2 - self.__dict__[1] ** 2 - self.__dict__[2] ** 2 + self.__dict__[3] ** 2)
-        pitch = np.arcsin(2 * (self.__dict__[1] * self.__dict__[3] + self.__dict__[0] * self.__dict__[1]))
-        yaw = np.arctan2(-2 * (self.__dict__[1] * self.__dict__[2] - self.__dict__[0] * self.__dict__[3]), self.__dict__[0] ** 2 + self.__dict__[1] ** 2 - self.__dict__[2] ** 2 - self.__dict__[3] ** 2)
+        roll = np.arctan2(-2 * (self[2] * self[3] - self[0] * self[1]), self[0] ** 2 - self[1] ** 2 - self[2] ** 2 + self[3] ** 2)
+        pitch = np.arcsin(2 * (self[1] * self[3] + self[0] * self[1]))
+        yaw = np.arctan2(-2 * (self[1] * self[2] - self[0] * self[3]), self[0] ** 2 + self[1] ** 2 - self[2] ** 2 - self[3] ** 2)
         return roll, pitch, yaw
 
     def __mul__(self, other):
@@ -131,13 +132,13 @@ class Quaternion:
 
     # Implementing other interfaces to ease working with the class
 
-    def _set_q(self, q):
-        self._q = q
-
-    def _get_q(self):
+    @property
+    def q(self):
         return self._q
 
-    q = property(_get_q, _set_q)
+    @q.setter
+    def q(self, q):
+        self._q = q
 
     def __getitem__(self, item):
         return self._q[item]